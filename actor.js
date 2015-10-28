975d97418bc7f6af1c1d0d7e01029936e39bb306
Nate <nate@nate.fr>
2015-10-28 16:05:24 -0400

    actors' movies

--- actor.js
+++ actor.js
@@ -2,7 +2,15 @@
  * Created by Nate on 15-10-28.
  */
 
-
+ActorsMovies = Backbone.Collection.extend({
+    url: function () {
+        return 'https://umovie.herokuapp.com/actors/' + this.artistId + '/movies';
+    },
+    model: Movie,
+    parse: function (response) {
+        return response.results;
+    }
+});
 
 Actor = Backbone.Model.extend({
     urlRoot: 'https://umovie.herokuapp.com/actors/',
@@ -10,15 +18,22 @@ Actor = Backbone.Model.extend({
         artworkUrl: '',
         movies: []
     },
+    idAttribute: "artistId",
     parse: function (response) {
         if (response.resultCount != 1) {
             alert("Actor not found");
             return;
         }
         response = response.results[0];
-        response.id = response.artistId;
-        response.artworkUrl = this.getActorPicture(response.artistName);
+        this.getActorPicture(response.artistName);
         return response;
+    },
+    getActorMovies: function () {
+        var self = this;
+        var movies = new ActorsMovies([], {artistId: this.get(idAttribute)});
+        movies.fetch().success(function (data) {
+            setActorMovies(data.models);
+        })
     },
     getActorPicture: function (actorName) {
         var self = this;
@@ -33,6 +48,10 @@ Actor = Backbone.Model.extend({
     },
     setActorPicture: function (picture) {
         this.set("artworkUrl", picture);
+        this.trigger("sync", this);
+    },
+    setActorMovies: function (movies) {
+        this.set("movies", movies);
         this.trigger("sync", this);
     }
 });
--- movie.js
+++ movie.js
@@ -4,13 +4,13 @@
 
 Movie = Backbone.Model.extend({
     urlRoot: 'https://umovie.herokuapp.com/movies/',
+    idAttribute: 'trackId',
     parse: function (response) {
         if (response.resultCount != 1) {
             alert("Movie not found");
             return;
         }
         response = response.results[0];
-        response.id = response.trackId;
         response.releaseDate = new Date(response.releaseDate);
         return response;
     }
