1cfec32e4209f6d7ac178b798938d2a8526e265c
Nate <nate@nate.fr>
2015-11-23 17:09:50 -0500

    Cleanup

--- test-bootstrap.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function() {
-    //showMovie(265727087);
-    //showActor(272994458);
-    showWatchLists();
-});
--- utilities.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by marvin on 2015-11-02.
- */
-
-function parseYoutubeWatchLinkToEmbed(watchLink){
-    return watchLink.replace('watch?v=', 'embed/');
-}
