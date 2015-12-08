d540c74028905c36442931880989f684c981efdc
MedAziz1 <mdacheche@gmail.com>
2015-12-07 19:01:38 -0500

    Filter by genre

--- app/scripts/models/searchGlobal.js
+++ app/scripts/models/searchGlobal.js
@@ -1,10 +1,13 @@
 define(['jquery', 'underscore', 'backbone','collections/searchActors','collections/searchMovies','collections/searchSeries', 'libraries/authentification'], function($, _,  Backbone, SearchActors, SearchMovies, SearchSeries, auth) {
 
+
     SearchGlobal = Backbone.Model.extend({
 
         actors: new SearchActors(),
         movies: new SearchMovies(),
         series: new SearchSeries(),
+        genres: [],
+
 
         parse: function(data){
             return data.results;
@@ -13,12 +16,52 @@ define(['jquery', 'underscore', 'backbone','collections/searchActors','collectio
             var self = this;
 
             if(typeof options == 'undefined') options = {};
-            var onSuccess = function(){self.trigger('sync', self);}
+            var onSuccess = function(type){
+                self.trigger('sync', self);
+                self.getListOfGenres(type);
 
-            this.actors.Search(searchQuery, options, onSuccess);
-            this.movies.Search(searchQuery, options, onSuccess);
-            this.series.Search(searchQuery, options, onSuccess);
+            }
+            this.actors = new SearchActors();
+            this.movies = new SearchMovies();
+            this.series = new SearchSeries();
+            this.genres = [];
+            this.actors.Search(searchQuery, options, function(){onSuccess("actors")});
+            this.movies.Search(searchQuery, options, function(){onSuccess("movies")});
+            this.series.Search(searchQuery, options,  function(){onSuccess("series")});
 
+
+        },
+
+        getListOfGenres: function(type){
+            if(type == "actors"){
+                var actorGenres = this.actors.map(function(model){
+                    return model.get('primaryGenreName');
+                });
+                this.genres = this.genres.concat(actorGenres);
+            }
+            if(type == "movies"){
+                var moviesGenres = this.movies.map(function(model){
+                    return model.get('primaryGenreName');
+                });
+                this.genres = this.genres.concat(moviesGenres);
+            }
+            if(type == "series"){
+                var seriesGenres = this.series.map(function(model){
+                    return model.get("primaryGenreName")
+                });
+                this.genres = this.genres.concat(seriesGenres);
+            }
+
+            this.getGenresWithoutDuplicates();
+
+        },
+
+        getGenresWithoutDuplicates: function(){
+            var uniqueGenres = [];
+            $.each(this.genres, function(i, el){
+                if($.inArray(el, uniqueGenres) === -1) uniqueGenres.push(el);
+            });
+            this.genres = uniqueGenres;
         }
     });
 
